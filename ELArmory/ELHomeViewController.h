e8a616830c55a981a3a63360c399211af22c7a32
Er Li <er.li@catalinamarketing.com>
2014-06-25 15:57:22 -0400

    add home view controller

---
+++ ELArmory/ELHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ELHomeViewController.h
+//  ELArmory
+//
+//  Created by Er Li on 6/25/14.
+//  Copyright (c) 2014 er.li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELHomeViewController : UIViewController
+
+@end
---
+++ ELArmory/ELHomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  ELHomeViewController.m
+//  ELArmory
+//
+//  Created by Er Li on 6/25/14.
+//  Copyright (c) 2014 er.li. All rights reserved.
+//
+
+#import "ELHomeViewController.h"
+
+@interface ELHomeViewController ()
+
+@end
+
+@implementation ELHomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
